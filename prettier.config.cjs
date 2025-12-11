400b4610fad29e4f68b3f55ebe304bd8fa90e476
George Nick Gorzynski <georgegorzynski@me.com>
2025-12-11 02:35:33 +0000

    feat: add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  printWidth: 110,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
